53b5a16d1f8f05195655a8ce4db9f77305dd9824
Evan <holland200072@gmail.com>
2025-08-16 13:39:11 -0400

    initial commit

---
+++ src/main/java/com/piggybank/vault/MoneyVaultApplication.java
@@ -0,0 +1,13 @@
+package com.piggybank.vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piggybank/vault/MoneyVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piggybank.vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
